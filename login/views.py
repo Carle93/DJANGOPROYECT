f603fac577de8d2580d2206a028e8261072e63fb
carlexi niño <isley7777@gmail.com>
2023-09-06 18:31:25 -0400

    a litter

--- login/views.py
+++ login/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -8,3 +11,19 @@ def home(request):
 
 def services(request):
     return render(request,"login/services.html")
+
+
+def registro(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('inicio_sesion')  # Redirige al inicio de sesión después del registro
+    else:
+        form = UserCreationForm()
+    return render(request, 'registro.html', {'form': form})
+
+@login_required
+def mi_perfil(request):
+    # Esta vista está protegida y solo es accesible para usuarios autenticados
+    return render(request, 'mi_perfil.html')
