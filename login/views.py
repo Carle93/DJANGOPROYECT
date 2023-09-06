65794d53190dfefbf4e009bd8fa451455c0b737f
carlexi niño <isley7777@gmail.com>
2023-09-05 20:49:35 -0400

    a litter

--- login/views.py
+++ login/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth.urls import login_required 
+
 
 # Create your views here.
 def home(request):
     return render(request,"login/home.html")
 
-@login_required
+
 def services(request):
     return render(request,"login/services.html")
