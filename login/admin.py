8853bd7f528cd374587d24ac8c9fbef33e37514a
carlexi niño <isley7777@gmail.com>
2024-02-28 15:58:18 +0100

    super

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Users
+from .models import Moods, Test, Files
 
-# Register your models here.
+admin.site.register(Users)
+admin.site.register(Moods)
+admin.site.register(Test)
+admin.site.register(Files)
