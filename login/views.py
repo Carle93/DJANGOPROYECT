993d8cc053aeff8cba694cac46b4430253e90dda
carlexi niño <isley7777@gmail.com>
2024-05-15 17:41:17 +0200

    sincronizacion with imagen in moods

--- login/views.py
+++ login/views.py
@@ -6,12 +6,12 @@ from .models import Moods
 
 # Create your views here.
 def home(request):
-    moods = Moods.objects.filter(soft_delete=False).order_by('?')[:3] #filtre only moods that active and #oder_by is to get aleatorio result and It was limited only 3
+    moods = Moods.objects.filter(soft_delete=False).order_by('?')[:3]
+
     for mood in moods:
-        mood.image_url = mood.imagens.url if mood.imagens else None
+        mood.imagens_url = mood.imagens.url if mood.imagens else None
 
     return render(request, 'login/home.html', {'moods': moods})
-    
    
 def test(request):
     return render(request,"login/test.html")
